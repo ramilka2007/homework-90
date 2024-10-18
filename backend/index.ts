673c73dfcbda9b80a7cff75d652eb3cf7a1f490a
ramilka <lilya1976.03.03@gmail.com>
2024-10-19 04:42:49 +0600

    Сделал базовую разметку вебсокета.

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import expressWs from 'express-ws';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import type {ActiveConnections, IncomingMessage} from './types';
 import config from "./config";
 
 const app = express();
@@ -12,6 +13,43 @@ app.use(cors(config.corsOptions));
 app.use(express.json());
 
 const router = express.Router();
+
+const activeConnections: ActiveConnections = {};
+
+router.ws('/chat',  (ws, _req) => {
+    const id = crypto.randomUUID();
+    console.log('client connected! id=', id);
+    activeConnections[id] = ws;
+
+    ws.on('message', (msg) => {
+        const decodedMessage = JSON.parse(msg.toString()) as IncomingMessage;
+
+        switch (decodedMessage.type) {
+            case 'SET_USERNAME':
+                username = decodedMessage.payload;
+                break;
+            case 'SEND_MESSAGE':
+                Object.keys(activeConnections).forEach(connId => {
+                    const conn = activeConnections[connId];
+                    conn.send(JSON.stringify({
+                        type: 'NEW_MESSAGE',
+                        payload: {
+                            username,
+                            text: decodedMessage.payload
+                        }
+                    }));
+                });
+                break;
+            default:
+                console.log('Unknown message type:', decodedMessage.type);
+        }
+    });
+
+    ws.on('close', () => {
+        console.log('client disconnected! id=', id);
+        delete activeConnections[id];
+    });
+});
 
 app.use(router);
 
---
+++ backend/types.ts
@@ -0,0 +1,10 @@
+import {WebSocket} from 'ws';
+
+export interface ActiveConnections {
+    [id: string]: WebSocket
+}
+
+export interface IncomingMessage {
+    type: string;
+    payload: string;
+}
